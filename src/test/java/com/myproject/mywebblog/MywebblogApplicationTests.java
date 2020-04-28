6526a17c1cb1dd4076f8c82d1b89ae33dbc691b0
Maksym Rotar <maxim20003244@gmail.com>
2020-04-28 01:12:07 +0100

    Initial commit

---
+++ src/test/java/com/myproject/mywebblog/MywebblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.mywebblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
