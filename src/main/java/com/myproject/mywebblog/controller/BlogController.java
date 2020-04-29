110423dbae14b5cc937953810397d52c979f988e
Maksym Rotar <maxim20003244@gmail.com>
2020-04-29 22:00:59 +0100

    -Output of each article. -Add some logo. -Add search bar.

--- src/main/java/com/myproject/mywebblog/controller/BlogController.java
+++ src/main/java/com/myproject/mywebblog/controller/BlogController.java
@@ -2,12 +2,17 @@ package com.myproject.mywebblog.controller;
 
 import com.myproject.mywebblog.models.Post;
 import com.myproject.mywebblog.controller.repo.PostRepository;
+import jdk.nashorn.internal.runtime.regexp.joni.Option;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 @Controller
@@ -33,5 +38,16 @@ public class BlogController {
         Post post = new Post(title,anons,full_text);
         postRepository.save(post);
         return "redirect:/blog";
+    }
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+         Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post" ,res);
+        return "blog-details";
     }
 }
